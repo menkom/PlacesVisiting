5a57633597ba6a5c692174d02758f20e54101ae5
Mike <39326256+menkom@users.noreply.github.com>
2024-04-22 08:55:00 +0200

    Added OAuth Google authentication and let basic and OAuth work together

--- src/main/java/info/mastera/placesVisiting/config/WebSecurityConfig.java
+++ src/main/java/info/mastera/placesVisiting/config/WebSecurityConfig.java
@@ -1,24 +1,25 @@
 package info.mastera.placesVisiting.config;
 
 import info.mastera.placesVisiting.filter.JwtFilter;
+import info.mastera.placesVisiting.service.OAuth2UserService;
 import info.mastera.placesVisiting.service.UserService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
-import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
+import static org.springframework.security.config.http.SessionCreationPolicy.IF_REQUIRED;
 
 @Configuration
 @EnableWebSecurity
@@ -31,11 +32,6 @@ public class WebSecurityConfig {
     }
 
     @Bean
-    public UserDetailsService userDetailsService(UserService accountService) {
-        return accountService;
-    }
-
-    @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
@@ -43,23 +39,45 @@ public class WebSecurityConfig {
     @Bean
     public AuthenticationProvider authenticationProvider(UserService userService) {
         DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
-        authProvider.setUserDetailsService(userDetailsService(userService));
+        authProvider.setUserDetailsService(userService);
         authProvider.setPasswordEncoder(passwordEncoder());
         return authProvider;
     }
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http, UserService userService, JwtFilter jwtFilter) throws Exception {
+    public SecurityFilterChain securityFilterChain(HttpSecurity http,
+                                                   UserService userService,
+                                                   JwtFilter jwtFilter,
+                                                   OAuth2UserService oAuth2UserService) throws Exception {
         return http
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(request ->
                         request
-                                .requestMatchers("/favicon.ico", "/account/*").permitAll()
+                                .requestMatchers(
+                                        "/",
+                                        "/favicon.ico",
+                                        "/account/*",
+                                        "/login**",
+                                        "/error").permitAll()
                                 .anyRequest().authenticated()
                 )
-                .sessionManagement(manager -> manager.sessionCreationPolicy(STATELESS))
+                .sessionManagement(manager -> manager.sessionCreationPolicy(IF_REQUIRED))
+                .formLogin(formLogin -> formLogin
+                        .loginPage("/login")
+                        .permitAll()
+                )
+                .httpBasic(Customizer.withDefaults())
                 .authenticationProvider(authenticationProvider(userService))
-                .logout(LogoutConfigurer::permitAll)
+                .oauth2Login(oauth2 -> oauth2.userInfoEndpoint(infoEndpoint -> infoEndpoint.userService(oAuth2UserService)))
+                .logout(logout -> logout
+                        .invalidateHttpSession(true)
+                        .deleteCookies("JSESSIONID")
+                        .clearAuthentication(true)
+                        .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+                )
+                .oidcLogout((logout) -> logout
+                        .backChannel(Customizer.withDefaults())
+                )
                 .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
     }
---
+++ src/main/java/info/mastera/placesVisiting/dto/UserDto.java
@@ -0,0 +1,6 @@
+package info.mastera.placesVisiting.dto;
+
+import info.mastera.placesVisiting.model.Provider;
+
+public record UserDto(String username, Provider provider) {
+}
