2719b0b18c1673364425ec973555536dc676b99c
Mike <39326256+menkom@users.noreply.github.com>
2024-04-22 08:52:26 +0200

    Minor changes to test controller and HTTP scratch

--- src/main/java/info/mastera/placesVisiting/controller/TestController.java
+++ src/main/java/info/mastera/placesVisiting/controller/TestController.java
@@ -1,16 +1,27 @@
 package info.mastera.placesVisiting.controller;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/test")
+@RequestMapping("/")
 public class TestController {
 
-    @GetMapping
+    @GetMapping("test")
     public ResponseEntity<String> test() {
         return ResponseEntity.ok("You are authenticated");
+    }
+
+    @GetMapping
+    public ResponseEntity<String> root() {
+        return ResponseEntity.ok("No need authentication");
+    }
+
+    @GetMapping("/principal")
+    public Object getPrincipal(Authentication authentication) {
+        return authentication.getPrincipal();
     }
 }
