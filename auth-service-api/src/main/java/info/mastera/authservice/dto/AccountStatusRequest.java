596505ba35c914874636c9375ba000551573b586
m.menko <39326256+menkom@users.noreply.github.com>
2024-07-08 11:41:10 +0200

    Added ability to serialize record to JSON

--- auth-service-api/src/main/java/info/mastera/authservice/dto/AccountStatusRequest.java
+++ auth-service-api/src/main/java/info/mastera/authservice/dto/AccountStatusRequest.java
@@ -1,5 +1,10 @@
 package info.mastera.authservice.dto;
 
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+
+@JsonSerialize
+@JsonAutoDetect(fieldVisibility = JsonAutoDetect.Visibility.ANY)
 public record AccountStatusRequest(
         Long userId,
         String username
--- auth-service-api/src/main/java/info/mastera/authservice/dto/LoginRequest.java
+++ auth-service-api/src/main/java/info/mastera/authservice/dto/LoginRequest.java
@@ -1,6 +1,10 @@
 package info.mastera.authservice.dto;
 
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import jakarta.validation.constraints.NotEmpty;
 
+@JsonSerialize
+@JsonAutoDetect(fieldVisibility = JsonAutoDetect.Visibility.ANY)
 public record LoginRequest(@NotEmpty String username, @NotEmpty String password) {
 }
